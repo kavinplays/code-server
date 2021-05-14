fec2ca1fdf1765098c11ae874faf0c0f2638bf8a
kavinplays <59177133+kavinplays@users.noreply.github.com>
2021-05-14 11:58:59 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 START_DIR="${START_DIR:-/home/coder/code-server}"
-REQUIRED_DIR="${REQUIRED_DIR:-/home/coder/discord-bot}"
+REQUIRED_DIR="${REQUIRED_DIR:-/home/coder/cpp}"
 PREFIX="deploy-code-server"
 
 mkdir -p $START_DIR
 
 # function to clone the git repo or add a user's first file if no repo was specified.
 project_init () {
-    [ -z "${GIT_REPO}" ] && echo "[$PREFIX] No GIT_REPO specified" && echo "Example file. Have questions? Join us at https://community.coder.com" > $START_DIR/coder.txt || git clone $GIT_REPO $START_DIR && sudo git clone -b staging ${GIT_PRIVATE} && sudo chown -R coder $REQUIRED_DIR
+    [ -z "${GIT_REPO}" ] && echo "[$PREFIX] No GIT_REPO specified" && echo "Example file. Have questions? Join us at https://community.coder.com" > $START_DIR/coder.txt || git clone $GIT_REPO $START_DIR && sudo git clone ${GIT_PRIVATE} && sudo chown -R coder $REQUIRED_DIR
 }
 
 # add rclone config and start rclone, if supplied
