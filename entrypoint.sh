9143b8376d1ee17820069f9454feb6cc78d20b72
kavinplays <59177133+kavinplays@users.noreply.github.com>
2021-08-08 20:31:46 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,12 +7,12 @@ mkdir -p $START_DIR
 
 # function to clone the git repo or add a user's first file if no repo was specified.
 project_init () {
-    sudo mkdir $START_DIR/projects && cd $START_DIR/.deta/ && sudo touch tokens 
+    sudo mkdir $START_DIR/projects && cd $START_DIR/.deta/ && sudo touch tokens &&
     sudo cat > tokens <<EOF
-    >{
-    >   "deta_access_token": "${DETA_ACCESS_TOKEN}"
-    >}
-    >EOF
+    {
+       "deta_access_token": "${DETA_ACCESS_TOKEN}"
+    }
+    EOF
 }
 
 # add rclone config and start rclone, if supplied
