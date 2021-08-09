09aa002d933a8e740864b7244753c3a0efc7d4ba
kavinplays <59177133+kavinplays@users.noreply.github.com>
2021-08-10 00:25:43 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ PREFIX="deploy-code-server"
 mkdir -p $START_DIR
 
 project_init () {
-    sudo mkdir $START_DIR/projects
+    sudo mkdir $START_DIR/projects && sudo echo -e "Username:kavinplays\nPassword:${GITHUB}" > $START_DIR/projects/github_login
 }
 project_init
 echo "[$PREFIX] Starting code-server..."
